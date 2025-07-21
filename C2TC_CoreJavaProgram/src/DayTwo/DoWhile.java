9f8ecf5ee3413fb1a8c64f145f3f84e60f9ac4ec
navvin's acer <navvin's acer@navvin.lan>
2025-07-21 21:57:56 +0530

    DoWhile

---
+++ C2TC_CoreJavaProgram/src/DayTwo/DoWhile.java
@@ -0,0 +1,13 @@
+package DayTwo;
+
+public class DoWhile {
+	public class DoWhileDemo {
+		public static void main(String[] args) {
+			int i = 11;
+			do {
+				System.out.println(i);
+				i++;
+			} while (i <= 1);
+		}
+}
+}
