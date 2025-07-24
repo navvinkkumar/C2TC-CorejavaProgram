13ef7fb9303d5411b6fd136637128ae0c0f753df
navvin's acer <navvin's acer@navvin.lan>
2025-07-24 20:34:02 +0530

    new

---
+++ C2TC_CoreJavaProgram/src/SecondPackage/Executor.java
@@ -0,0 +1,10 @@
+package SecondPackage;
+import com.tnsif.dayfour.Base;
+public class Executor {
+	public static void main(String[] args) {
+		Base b1 = new Base();
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+}
